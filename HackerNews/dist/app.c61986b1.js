ce4b17307e5887db4e12748a47e29096b2aa0939
eunbin-lee <euns2bin@gmail.com>
2021-07-08 15:50:05 +0900

    TS | 함수의 규격 작성하기 (REST Client, 제네릭)

--- HackerNews/dist/app.c61986b1.js
+++ HackerNews/dist/app.c61986b1.js
@@ -129,6 +129,10 @@ var store = {
   currentPage: 1,
   feeds: []
 };
+/*
+Generics: 호출하는 쪽에서 유형을 명시(ex. getData<NewsFeed[]>(NEWS_URL))해주면
+그 유형(NewsFeed[])을 받아서 반환 유형으로 사용할 수 있다
+*/
 
 function getData(url) {
   ajax.open('GET', url, false);
@@ -166,8 +170,8 @@ function newsFeed() {
   }
 
   template = template.replace('{{__news_feed__}}', newsList.join(''));
-  template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
-  template = template.replace('{{__next_page__}}', store.currentPage + 1);
+  template = template.replace('{{__prev_page__}}', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
+  template = template.replace('{{__next_page__}}', String(store.currentPage + 1));
   updateView(template);
 }
 
@@ -183,25 +187,22 @@ function newsDetail() {
     }
   }
 
-  function makeComment(comments, called) {
-    if (called === void 0) {
-      called = 0;
+  updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+}
+
+function makeComment(comments) {
+  var commentString = [];
+
+  for (var i = 0; i < comments.length; i++) {
+    var comment = comments[i];
+    commentString.push("\n      <div style=\"padding-left: " + comment.level * 40 + "px;\" class=\"mt-4\">\n        <div class=\"text-gray-400\">\n          <i class=\"fa fa-sort-up mr-2\"></i>\n          <strong>" + comment.user + "</strong> " + comment.time_ago + "\n        </div>\n        <p class=\"text-gray-700\">" + comment.content + "</p>\n      </div> \n    ");
+
+    if (comment.comments.length > 0) {
+      commentString.push(makeComment(comment.comments));
     }
-
-    var commentString = [];
-
-    for (var i = 0; i < comments.length; i++) {
-      commentString.push("\n        <div style=\"padding-left: " + called * 40 + "px;\" class=\"mt-4\">\n          <div class=\"text-gray-400\">\n            <i class=\"fa fa-sort-up mr-2\"></i>\n            <strong>" + comments[i].user + "</strong> " + comments[i].time_ago + "\n          </div>\n          <p class=\"text-gray-700\">" + comments[i].content + "</p>\n        </div> \n      ");
-
-      if (comments[i].comments.length > 0) {
-        commentString.push(makeComment(comments[i].comments, called + 1));
-      }
-    }
-
-    return commentString.join('');
   }
 
-  updateView(template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+  return commentString.join('');
 }
 
 function router() {
