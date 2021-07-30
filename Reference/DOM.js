7b83719ab2f32db348c301fdf358ae8a8f5ce042
eunbin-lee <euns2bin@gmail.com>
2021-07-30 19:51:35 +0900

    참조 | DOM

---
+++ Reference/DOM.js
@@ -0,0 +1,7 @@
+/* 
+[ DOM (Document Object Model) ]
+: html이라고 하는 문자열 데이터를 브라우저가 처리하기 위해 자체적으로 갖고 있는 자료구조
+
+[ 참고 자료 ]
+https://developer.mozilla.org/ko/docs/Web/API/Document_Object_Model/Introduction
+*/
